b07fe346698916ac48d77b8a110ce052b961176c
Juliet <julietaesterasrt@gmail.com>
2024-07-09 22:13:04 -0300

    resuelto el editar

--- js/usuario_editar.js
+++ js/usuario_editar.js
@@ -9,7 +9,7 @@ const { createApp } = Vue
         nombre:"",
         clave:"",
         nivel:"",
-        url:'http://127.0.0.1:5000/usuarios/'+id,
+        url:'https://natsanabria.pythonanywhere.com/usuarios/'+id,
        }  
     },
     methods: {
